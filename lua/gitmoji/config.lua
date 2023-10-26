4bc6d52f8bc78bf6296b523645c032e5d0680d2b
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2023-10-27 01:23:18 +0200

    chore(config): :wrench: added new configuration options

--- lua/gitmoji/config.lua
+++ lua/gitmoji/config.lua
@@ -1,3 +1,18 @@
+---@alias gitmoji.complete_type
+---| "emoji"
+---| "text"
+
+---@class gitmoji.completion
+---@field append_space boolean append space after completion?
+---@field complete_as gitmoji.complete_type complete as emoji or text?
+
+---@class gitmoji.config
+---@field filetypes string[] array of filetypes to add gitmoji completions for.
+---@field complete_as gitmoji.completion
 return {
-	filetypes = { "gitcommit" },
+    filetypes = { "gitcommit" },
+    completion = {
+        append_space = false,
+        complete_as = "emoji",
+    },
 }
