e3e691b3e10db1b3ca0d0ba8e890631950f76683
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2023-10-27 01:29:34 +0200

    feat(completion): :sparkles: emoji or texts
    It is now possible to select either to insert the emoji or the text
    representation for the emoji.
    
    See configs.

--- lua/gitmoji.lua
+++ lua/gitmoji.lua
@@ -9,40 +9,48 @@ function M.setup(opts)
 end
 
 function M.get_source()
-	local source = {}
+    local source = {}
 
-	source.is_available = function()
-		local ft = vim.bo.filetype
-		return vim.tbl_contains(gitmoji_config.filetypes, ft)
-	end
+    source.is_available = function()
+        local ft = vim.bo.filetype
+        return vim.tbl_contains(gitmoji_config.filetypes, ft)
+    end
 
-	source.new = function()
-		local self = setmetatable({}, { __index = source })
-		self.items = nil
-		return self
-	end
+    source.new = function()
+        local self = setmetatable({}, { __index = source })
+        self.items = nil
+        return self
+    end
 
-	source.get_trigger_characters = function()
-		return { ":" }
-	end
+    source.get_trigger_characters = function()
+        return { ":" }
+    end
 
-	source.get_keyword_pattern = function()
-		return [[\%(\s\|^\)\zs:\w*:\?]]
-	end
+    source.get_keyword_pattern = function()
+        return [[\%(\s\|^\)\zs:\w*:\?]]
+    end
 
-	source.complete = function(self, request, callback)
-		-- Avoid unexpected completion.
-		if not vim.regex(self.get_keyword_pattern() .. "$"):match_str(request.context.cursor_before_line) then
-			return callback()
-		end
+    source.complete = function(self, request, callback)
+        -- Avoid unexpected completion.
+        if not vim.regex(self.get_keyword_pattern() .. "$"):match_str(request.context.cursor_before_line) then
+            return callback()
+        end
 
-		if not self.items then
-			self.items = require("gitmoji.items")
-		end
+        if not self.items then
+            ---@type lsp.CompletionItem[]
+            self.items = {}
+            local gitmoji_items = require("gitmoji.items")
+            for _, item in ipairs(gitmoji_items) do
+                item.insertText = item[gitmoji_config.completion.complete_as]
+                    .. (gitmoji_config.completion.append_space and " " or "")
+                table.insert(self.items, item)
+            end
+        end
 
-		callback(self.items)
-	end
-	return source
+        callback(self.items)
+    end
+
+    return source
 end
 
 function M.source_name()
