001fb8a374ff63bce631de782ae6ab099f5047e8
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2025-04-06 12:27:49 +0200

    fix: tried to fix blink setup

--- after/plugin/gitmoji.lua
+++ after/plugin/gitmoji.lua
@@ -1,2 +1,5 @@
-local gitmoji = require("gitmoji")
-require("cmp").register_source(gitmoji.source_name(), gitmoji.get_source().new())
+local ok, cmp = pcall(require, "cmp")
+if ok then
+    local gitmoji = require("gitmoji")
+    cmp.register_source(gitmoji.source_name(), gitmoji.get_source().new())
+end
